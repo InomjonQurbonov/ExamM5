fec38908b652cba1cb0fc44acd7a9de4bae986d3
Inomjon Qurbonov <145829813+InomjonQurbonov@users.noreply.github.com>
2024-02-17 08:42:06 +0500

    1-savol:Fibonachchi sonlarini topuvchi dastur
    Ushbu dastur sizga fibonachchi sonlarini topishga yordam beradi.Dasturga shunchaki sizga keraklicha sonni yani nechta finonachchi soni kerakligini yozasiz va datur sizga shuncha finonachchi sonini taqdim etadi.
    Dastur yield() operatoridan foydalanib yozilgan
    Imtixon M5 ID19304 Najot Ta'lim

---
+++ 1-savol.py
@@ -0,0 +1,12 @@
+def generate_fibonacci(n):
+    fibonacci = [0, 1]
+    for i in range(2, n):
+        son = fibonacci[i-1] + fibonacci[i-2]
+        fibonacci.append(son)
+        yield son
+
+n = int(input("Fibonachchi sonlarining nechta elementini topasiz:"))
+
+print(f"Fibonachchi sonlari {n} ta elementi:")
+for son in generate_fibonacci(n):
+    print(son)
